5af21b8e2be5e08fd42cca9effbe0a67eb743c77
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-18 13:54:20 +0500

    Update systeminfo.py

--- tg_bot/modules/systeminfo.py
+++ tg_bot/modules/systeminfo.py
@@ -12,24 +12,6 @@ from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot import dispatcher ,DEV_USERS
 from tg_bot.modules.helper_funcs.filters import CustomFilters
 
-def speed_convert(size):
-    power = 2 ** 10
-    zero = 0
-    units = {0: '', 1: 'Kb/s', 2: 'Mb/s', 3: 'Gb/s', 4: 'Tb/s'}
-    while size > power:
-        size /= power
-        zero += 1
-    return f"{round(size, 2)} {units[zero]}"
-
-def convert(speed):
-    return round(int(speed)/1048576, 2)
-
-def get_size(bytes, suffix="B"):
-    factor = 1024
-    for unit in ["", "K", "M", "G", "T", "P"]:
-        if bytes < factor:
-            return f"{bytes:.2f}{unit}{suffix}"
-        bytes /= factor
 
 @dev_plus
 @run_async
@@ -37,7 +19,7 @@ def status(bot: Bot, update: Update):
 	chat = update.effective_chat
 	
 	stat = "--- System Status ---\n"
-	stat += "saber Version: `5.5.2`""\n"
+	stat += "lynda Version: `5.5.2`""\n"
 	stat += "Python Version: `"+python_version()+"`\n"
 	stat += "GitHub API Version: `"+str(git.vercheck())+"`\n"
 	#Software Info
@@ -78,50 +60,6 @@ def status(bot: Bot, update: Update):
 	bot.send_message(chat.id, reply, parse_mode=ParseMode.MARKDOWN)
 
 
-
-@dev_plus
-@run_async
-def speedtestxyz(bot: Bot, update: Update):
-    buttons = [
-        [InlineKeyboardButton("Image", callback_data="speedtest_image"), InlineKeyboardButton("Text", callback_data="speedtest_text")]
-    ]
-    update.effective_message.reply_text("Select SpeedTest Mode",
-                                        reply_markup=InlineKeyboardMarkup(buttons))
-
-
-@run_async
-def speedtestxyz_callback(bot: Bot, update: Update):
-    query = update.callback_query
-
-    if query.from_user.id in DEV_USERS:
-        msg = update.effective_message.edit_text('Runing a speedtest....') 
-        speed = speedtest.Speedtest()
-        speed.get_best_server()
-        speed.download()
-        speed.upload()
-        replymsg = 'SpeedTest Results:'
-
-        if query.data == 'speedtest_image':
-            speedtest_image = speed.results.share()
-            update.effective_message.reply_photo(photo=speedtest_image, caption=replymsg)
-            msg.delete()
-
-        elif query.data == 'speedtest_text':
-            result = speed.results.dict()
-            replymsg += f"\nDownload: `{convert(result['download'])}Mb/s`\nUpload: `{convert(result['upload'])}Mb/s`\nPing: `{result['ping']}`"
-            update.effective_message.edit_text(replymsg, parse_mode=ParseMode.MARKDOWN)
-    else:
-        query.answer("You are required to join fate union to use this command.")
-
-
-
-
 STATUS_HANDLER = CommandHandler("system", status, filters=CustomFilters.sudo_filter)
-SPEED_TEST_HANDLER = DisableAbleCommandHandler("speedtest", speedtestxyz)
-SPEED_TEST_CALLBACKHANDLER = CallbackQueryHandler(speedtestxyz_callback, pattern='speedtest_.*')
-
 
 dispatcher.add_handler(STATUS_HANDLER)
-dispatcher.add_handler(SPEED_TEST_HANDLER)
-dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
-
